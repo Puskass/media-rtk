02d33aa8888b3df3655b2fe2391060024e4cb202
Puskass <cosovictarik711@gmail.com>
2023-11-25 12:26:03 +0100

    pause helper function

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,9 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { pause } from "../../utils/pause";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
 
+  await pause(1000); //DEV ONLY!!
   return response.data;
 });
 
