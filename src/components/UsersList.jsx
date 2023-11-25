f9fa6b45501ca817a6f8fec7f29e8bcab8f00908
Puskass <cosovictarik711@gmail.com>
2023-11-25 19:03:22 +0100

    deleting users not working

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from "../store";
 import Skeleton from "../common/Skeleton";
 import Button from "../common/Button";
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 const UsersList = () => {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -26,13 +27,7 @@ const UsersList = () => {
     content = <div>Error fetching data</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
     });
   }
 
---
+++ src/components/UsersListItem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { GoTrash } from "react-icons/go";
+import { removeUser } from "../store";
+import { useThunk } from "../hooks/use-thunk";
+import Button from "../common/Button";
+
+const UsersListItem = ({ user }) => {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleRemoveUser = () => {
+    doRemoveUser();
+  };
+
+  return (
+    <div className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        <div className="flex flex-row items-center justify-between ">
+          <Button
+            loading={isLoading}
+            className="mr-3"
+            onClick={handleRemoveUser}
+          >
+            <GoTrash />
+          </Button>
+          {error && <div>Error deleting user.</div>}
+          {user.name}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UsersListItem;
